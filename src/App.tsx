7d59cd86dab8f79a25d5ba630d6764ba706bfe06
James Webb <jameswebbdev@gmail.com>
2023-05-09 21:52:21 -0700

    Add features per take home prompt
    - The cards containing company data are centered on the page
    - Each card displays the company name, company state, plan year and employee count
    - Clicking "Show More" should display premium sum, broker commission and number of participants

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import companies from './data.json';
 
 function App() {
+  const firstShown: {[index: string]:any} = {}
+  const [shown, setShown] = useState(firstShown);
+
+  const showHide = (index: string | number) => {
+    setShown({...shown, [index + ""]: !shown[index + ""]});
+  }
+
+  useEffect(() => {
+    //set the "shown" object so we have shown/hidden statuses for each company
+    if(companies && companies.length > 0) {
+      let tempShown: {[index: string]:any} = {}
+      companies.forEach(function(company, i){
+        tempShown[i+""] = false;
+      });
+      setShown(tempShown);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {companies.map(function(company, i){
+        return (
+          <div className="card">
+            <div className="cardHeader">{company.company_name}</div>
+            <table className="cardTable">
+              <tr>
+                <td>State</td>
+                <td>Employees</td>
+                <td>Year</td>
+              </tr>
+              <tr>
+                <td className="cardTableBoldCell">{company.company_state}</td>
+                <td className="cardTableBoldCell">{company.employee_count}</td>
+                <td className="cardTableBoldCell">{company.plan_year}</td>
+              </tr>
+            </table>
+            <div className="showMore"><a href="#" onClick={() => showHide(i)}>{shown[i+""] ? "Show less ▲" : "Show more ▼"}</a></div>
+            <div className={shown[i+""] ? "moreSectionHidden" : "moreSection"}>
+              <div className="moreInnerContainer">
+                <div className="moreRow"><span style={{fontWeight: "bold"}}>Premium:</span> ${(company.premium_sum).toLocaleString("en-US")}</div>
+                <div className="moreRow"><span style={{fontWeight: "bold"}}>Participants:</span> {company.participants_sum}</div>
+                <div className="moreRow"><span style={{fontWeight: "bold"}}>Broker Commissions:</span> ${(company.broker_commission_sum).toLocaleString("en-US")}</div>
+              </div>
+            </div>
+          </div>
+        );
+        })
+      }
     </div>
   );
 }
